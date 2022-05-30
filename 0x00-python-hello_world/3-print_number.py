bc6069269ac615755984e4b7c7efffb174a316ea
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-05-30 10:20:45 -0700

    use of f-strings

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number} Battery street")
+print("{:d} Battery street".format(number))
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                  3,1           All
+
