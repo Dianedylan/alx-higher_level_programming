f4287fe2872119d671faa6fef55208dc68f55e03
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-06-01 00:24:26 -0700

    alphas and comb

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) == 'e' or chr(i) == 'q':
-        continue
-else:
-    print("{:s}".format(chr(i)), end="")
+for letter in range(97, 123):
+    if letter != 101 and letter != 113:
+        print("{:c}".format(letter), end='')
