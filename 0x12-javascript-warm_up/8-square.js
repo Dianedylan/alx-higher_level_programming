aa06a6a5e7a323dd31825c2d566454e2444442df
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-09-07 12:34:51 -0700

    JavaScript warm-up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-let num = process.argv[2];
-
-if (isNaN(num)) {
+if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < num; i++) {
-    let sqr = '';
-    for (let j = 0; j < num; j++) {
-      sqr = sqr + 'X';
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    let square = '';
+    for (let j = 0; j < parseInt(process.argv[2]); j++) {
+      square = square + 'X';
     }
-    console.log(sqr);
+    console.log(square);
   }
 }
